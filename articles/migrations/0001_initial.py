b69a4337bd749da13e39496643d7ad1495fdb5f0
xenddy <xenddy@naver.com>
2024-06-03 10:26:01 +0900

    commit

--- articles/migrations/0001_initial.py
+++ articles/migrations/0001_initial.py
@@ -25,21 +25,18 @@ class Migration(migrations.Migration):
         ),
     ]
 
-    migrations.CreateModel(
-            name='Comment',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('content', models.CharField(max_length=200)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('updated_at', models.DateTimeField(auto_now=True)),
-                ('article', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='comments', to='articles.article')),
-                ('content', models.CharField(max_length=255)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('updated_at', models.DateTimeField(auto_now=True)),
-            ],
-        ),
+migrations.CreateModel(
+    name='Comment',
+    fields=[
+        ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+        ('content', models.CharField(max_length=255)),  # 중복 제거하고 길이를 255로 수정
+        ('created_at', models.DateTimeField(auto_now_add=True)),
+        ('updated_at', models.DateTimeField(auto_now=True)),
+        ('article', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='comments', to='articles.article')),
+    ],
+),
 
-    migrations.CreateModel(
+migrations.CreateModel(
             name='Like',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
