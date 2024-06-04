bbc5134b2119ade0809ab6771c83691dacd99776
xenddy <xenddy@naver.com>
2024-06-04 15:09:21 +0900

    commit

--- articles/migrations/0001_initial.py
+++ articles/migrations/0001_initial.py
@@ -15,36 +15,45 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Article',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('category', models.CharField(choices=[('Travel', 'Travel'), ('Camping', 'Camping'), ('Leisure', 'Leisure'), ('Cooking', 'Cooking')], max_length=10)),
+                ('id', models.BigAutoField(auto_created=True,
+                primary_key=True, serialize=False, verbose_name='ID')),
+                ('category', models.CharField(choices=[('Travel', 'Travel'), ('Camping', 'Camping'), (
+                    'Leisure', 'Leisure'), ('Cooking', 'Cooking')], max_length=10)),
                 ('title', models.CharField(max_length=100)),
                 ('content', models.TextField()),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('author', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
     ]
 
+
 migrations.CreateModel(
     name='Comment',
     fields=[
-        ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+        ('id', models.BigAutoField(auto_created=True,
+        primary_key=True, serialize=False, verbose_name='ID')),
         ('content', models.CharField(max_length=255)),  # 중복 제거하고 길이를 255로 수정
         ('created_at', models.DateTimeField(auto_now_add=True)),
         ('updated_at', models.DateTimeField(auto_now=True)),
-        ('article', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='comments', to='articles.article')),
+        ('article', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+        related_name='comments', to='articles.article')),
     ],
 ),
 
 migrations.CreateModel(
-            name='Like',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('article', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='likes', to='articles.article')),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
-            ],
-            options={
-                'unique_together': {('user', 'article')},
-            },
-        )
+    name='Like',
+    fields=[
+        ('id', models.BigAutoField(auto_created=True,
+                                primary_key=True, serialize=False, verbose_name='ID')),
+        ('created_at', models.DateTimeField(auto_now_add=True)),
+        ('article', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+                                    related_name='likes', to='articles.article')),
+        ('user', models.ForeignKey(
+            on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+    ],
+    options={
+        'unique_together': {('user', 'article')},
+    },
+)
